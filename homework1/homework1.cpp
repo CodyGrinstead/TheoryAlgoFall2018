079fd1df1f83619c7ae99411b336a12f6c4d157c
CodyGrinstead <yodacodyone@gmail.com>
2018-08-29 12:40:36 -0600

    working kattis solution

--- homework1/homework1.cpp
+++ homework1/homework1.cpp
@@ -2,12 +2,51 @@
 //Homework 1
 //Theory of Algo
 
+/*
+Tim is an absolutely obsessive soda drinker, he simply cannot get enough. Most annoyingly though, he almost never has any money, so his only obvious legal way to obtain more soda is to take the money he gets when he recycles empty soda bottles to buy new ones. In addition to the empty bottles resulting from his own consumption he sometimes find empty bottles in the street. One day he was extra thirsty, so he actually drank sodas until he couldn’t afford a new one.
+
+Input
+Three non-negative integers e,f,c, where e<1000 equals the number of empty soda bottles in Tim’s possession at the start of the day, f<1000 the number of empty soda bottles found during the day, and 2≤c<2000 the number of empty bottles required to buy a new soda.
+
+Output
+How many sodas did Tim drink on his extra thirsty day?
+
+
+
+*/
 #include <fstream>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int sodadrinked(int e,int f, int c)
+{
+	int total;
+	int bought=(e+f)/c+(e+f)%c;
+	total=(e+f)/c;
+	while(bought >=c)
+	{
+		total+=bought/c;
+		bought= bought/c +bought%c;
+	}
+
+
+	return total;
+}
+
+
 
 
 
 int main(int argc, char *argv[]){
+	int numempty, numfound, costper;
+	int e, f, c;
+	cin >> numempty >> numfound >>costper;
+	
 
-
+	cout<<sodadrinked(numempty,numfound,costper)<<endl;
+	
 	return 0;
+
 }
